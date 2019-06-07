a62f8ed3cf8c456d7de7276cd973b99cc7fdd424
Mike Wuetherick <mikew@unity3d.com>
2019-06-07 13:32:02 -0700

    wrapped newcontentwizard with #if NEWCONTENTWIZARD while it's in development, need to finish a couple of small things before it becomes public

--- Editor/Module.NewContentWizard/NewContentWizardMenus.cs
+++ Editor/Module.NewContentWizard/NewContentWizardMenus.cs
@@ -9,7 +9,9 @@ namespace MWU.FilmLib
     {
         public const int DEFAULTMENUPRIORITY = -100;
 
+#if NEWCONTENTWIZARD
         [MenuItem(Loc.MENU_FILE_NEWSCENE, false, DEFAULTMENUPRIORITY)]
+#endif
         public static void NewSceneFromTemplate()
         {
             var thisWindow = EditorWindow.GetWindow<NewSceneWizardView>(false, Loc.WINDOWLABEL_WIZARD, true);
@@ -22,13 +24,17 @@ namespace MWU.FilmLib
         //    Debug.Log("Create new Project from template: Sorry not supported currently");
         //}
 
+#if NEWCONTENTWIZARD
         [MenuItem(Loc.MENU_TOOLS_EDITTEMPLATES, false, DEFAULTMENUPRIORITY)]
+#endif
         public static void EditContentTemplates()
         {
             var thisWindow = EditorWindow.GetWindow<TemplateEditorView>(false, Loc.WINDOWLABEL_TEMPLATE, true);
         }
 
+#if NEWCONTENTWIZARD
         [MenuItem(Loc.MENU_FILE_CONFIGUREPROJECT, false, DEFAULTMENUPRIORITY)]
+#endif
         public static void ConfigureProject()
         {
             if (EditorUtility.DisplayDialog(Loc.DIALOG_NEWPROJECTSETUP_TITLE,
@@ -40,14 +46,18 @@ namespace MWU.FilmLib
             }
         }
 
+#if NEWCONTENTWIZARD
         [MenuItem(Loc.MENU_ASSETS_POPULATEFOLDER, false, DEFAULTMENUPRIORITY)]
+#endif
         public static void PopulateFolderStructure()
         {
             var thisWindow = EditorWindow.GetWindow<NewSceneWizardView>(false, Loc.WINDOWLABEL_WIZARD, true);
             thisWindow.SetWizardType(TEMPLATE_TYPE.FOLDER);
         }
 
+#if NEWCONTENTWIZARD
         [MenuItem(Loc.MENU_GAMEOBJECT_POPULATESCENE, false, DEFAULTMENUPRIORITY)]
+#endif
         public static void PopulateSceneStructure()
         {
             var thisWindow = EditorWindow.GetWindow<NewSceneWizardView>(false, Loc.WINDOWLABEL_WIZARD, true);
