c0bb767a4aa6d9f1a95f7f637d5a928518cb72c7
John Parsaie <johnpa@unity3d.com>
2019-06-13 15:40:14 -0400

    Fur vertex refactor

--- Shaders/Module.Fur/FurVertex.hlsl
+++ Shaders/Module.Fur/FurVertex.hlsl
@@ -8,6 +8,19 @@ float3 Dynamics(AttributesMesh input)
     return 0;
 }
 
+// TODO: Note on comb map authoring.
+float3 GetCombTS(float2 texcoord)
+{
+    float4 combSample = SAMPLE_TEXTURE2D_LOD(_GroomCombMap, sampler_GroomCombMap, texcoord, 0.0);
+    return UnpackNormalmapRGorAG(combSample, COMB_STRENGTH);
+}
+
+float GetHeight(float2 texcoord)
+{
+    float height = SAMPLE_TEXTURE2D_LOD(_GroomHeightMap, sampler_GroomHeightMap, texcoord, 2);
+    return HEIGHT * height;//lerp(0.3, 1.0, smoothstep(0.2, 0.3, height));
+}
+
 AttributesMesh ApplyMeshModification(AttributesMesh input)
 {
 #if defined(ATTRIBUTES_NEED_NORMAL) && defined(ATTRIBUTES_NEED_TANGENT) && defined(ATTRIBUTES_NEED_TEXCOORD0) && defined(ATTRIBUTES_NEED_COLOR) 
@@ -17,13 +30,12 @@ AttributesMesh ApplyMeshModification(AttributesMesh input)
     float4 tangentWS = float4(TransformObjectToWorldDir(input.tangentOS.xyz), input.tangentOS.w);
     real3x3 worldToTangent = CreateWorldToTangent(normalWS, tangentWS.xyz, tangentWS.w);
 
-    float4 combSample = SAMPLE_TEXTURE2D_LOD(_GroomCombMap, sampler_GroomCombMap, input.uv0, 0.0);
-    float3 combDirectionTS = UnpackNormalmapRGorAG(combSample, COMB_STRENGTH);
-    combDirectionTS = float3(-combDirectionTS.x, combDirectionTS.yz);
+    // Comb direction
+    float3 combDirectionTS = GetCombTS(input.uv0.xy);
     float3 combDirectionWS = normalize(TransformTangentToWorld(combDirectionTS, worldToTangent));
 
-    float h = SAMPLE_TEXTURE2D_LOD(_GroomHeightMap, sampler_GroomHeightMap, input.uv0.xy, 2);
-    h = lerp(0.3, 1.0, smoothstep(0.2, 0.3 , h));
+    // Height
+    float height = GetHeight(input.uv0.xy);
 
     // Final position is derived from a quadratic blending function.
     {
@@ -35,7 +47,7 @@ AttributesMesh ApplyMeshModification(AttributesMesh input)
         float  U;
 
         //Blend inputs
-        D  = HEIGHT * 0.1 * (normalWS + 0);
+        D  = height * 0.1 * (normalWS + combDirectionWS);
         P0 = positionWS;
         P1 = P0 +  D;
         PC = P0 + (D * 0.5);
@@ -45,7 +57,7 @@ AttributesMesh ApplyMeshModification(AttributesMesh input)
         positionWS = (P0 * pow(1 - U, 2.0)) + (P1 * pow(U, 2.0)) + (PC * 2 * U * (1 - U));
 
         // We store strand tangents in vertex color.
-        U = _FurShellLayer + (1.0 / 128.0); // TODO: Send delta.
+        U = _FurShellLayer + (1.0 / 32.0); // TODO: Send delta.
         float3 nextShellPositionWS = (P0 * pow(1 - U, 2.0)) + (P1 * pow(U, 2.0)) + (PC * 2 * U * (1 - U));
 
         // TODO: Note on tangent calculation.
