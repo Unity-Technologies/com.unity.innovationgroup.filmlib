cfb954d5ba4aa5bc750a179ea2c51d02cfcaaffb
Mike Wuetherick <mikew@unity3d.com>
2019-06-15 19:01:14 +0200

    toggle gizmos menu

--- Editor/Module.Productivity/Utilities/Shortcuts.cs
+++ Editor/Module.Productivity/Utilities/Shortcuts.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEditor;
 using UnityEngine;
 using System.Collections.Generic;
+using System;
+using System.Reflection;
 
 namespace MWU.Shared.Utilities
 {
@@ -111,5 +113,39 @@ namespace MWU.Shared.Utilities
             }
         }
 
+        [MenuItem("Tools/Edit/Disable All Gizmos")]
+        static void DisableAllGizmosMenu()
+        {
+            ToggleGizmos(0);
+        }
+
+        [MenuItem("Tools/Edit/Enable All Gizmos")]
+        static void EnableAllGizmos()
+        {
+            ToggleGizmos(1);
+        }
+
+        static void ToggleGizmos(int state)
+        {
+            var Annotation = Type.GetType("UnityEditor.Annotation, UnityEditor");
+            var ClassId = Annotation.GetField("classID");
+            var ScriptClass = Annotation.GetField("scriptClass");
+
+            Type AnnotationUtility = Type.GetType("UnityEditor.AnnotationUtility, UnityEditor");
+            var GetAnnotations = AnnotationUtility.GetMethod("GetAnnotations", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static);
+            var SetGizmoEnabled = AnnotationUtility.GetMethod("SetGizmoEnabled", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static);
+            var SetIconEnabled = AnnotationUtility.GetMethod("SetIconEnabled", BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static);
+
+            Array annotations = (Array)GetAnnotations.Invoke(null, null);
+
+            foreach (var a in annotations)
+            {
+                int classId = (int)ClassId.GetValue(a);
+                string scriptClass = (string)ScriptClass.GetValue(a);
+
+                SetGizmoEnabled.Invoke(null, new object[] { classId, scriptClass, state });
+                SetIconEnabled.Invoke(null, new object[] { classId, scriptClass, state });
+            }
+        }
     }
 }
