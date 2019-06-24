5277fc83fc63037139da35c43cdf376116136cf2
Mike Wuetherick <mikew@unity3d.com>
2019-06-24 14:02:00 +0200

    simple gameobjectrecorder wrapper

---
+++ Editor/Module.TakeRecordingSystem/RecordTransformHierarchy/RecordTransformHierarchyEditor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+namespace MWU.FilmLib
+{
+    [CustomEditor(typeof(RecordTransformHierarchy))]
+    public class RecordTransformHierarchyEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+
+            var targetScript = (RecordTransformHierarchy)target;
+
+            if(targetScript.recordingActive)
+            {
+                if( GUILayout.Button("Stop Recording"))
+                {
+                    targetScript.StopRecording();
+                }
+            }
+            else
+            {
+                if (GUILayout.Button("Start Recording"))
+                {
+                    targetScript.StartRecording();
+                }
+            }
+        }
+    }
+}
---
+++ Runtime/Module.TakeRecordingSystem/RecordTransformHierarchy/recordtransformhierarchy.cs
@@ -0,0 +1,65 @@
+﻿using UnityEditor;
+using UnityEditor.Animations;
+using UnityEngine;
+
+namespace MWU.FilmLib
+{
+    public class RecordTransformHierarchy : MonoBehaviour
+    {
+        public AnimationClip clip;
+        public bool recordingActive = false;
+        public GameObject objectToRecord;
+
+        private GameObjectRecorder objectRecorder;
+
+        void Start()
+        {
+            // Create recorder and record the script GameObject.
+            objectRecorder = new GameObjectRecorder(objectToRecord);
+
+            // Bind all the Transforms on the GameObject and all its children.
+            objectRecorder.BindComponentsOfType<Transform>(objectToRecord, true);
+        }
+
+        public void StartRecording()
+        {
+            clip = new AnimationClip();
+
+            recordingActive = true;
+        }
+
+        public void StopRecording()
+        {
+            if (clip == null)
+                return;
+
+            if (objectRecorder.isRecording)
+            {
+                // Save the recorded session to the clip.
+                objectRecorder.SaveToClip(clip);
+
+                AssetDatabase.CreateAsset(clip, "Assets/Test.anim");
+            }
+
+            recordingActive = false;
+        }
+
+        void LateUpdate()
+        {
+            if (clip == null)
+                return;
+
+            if( recordingActive)
+            {
+                // Take a snapshot and record all the bindings values for this frame.
+                objectRecorder.TakeSnapshot(Time.deltaTime);
+            }
+            
+        }
+
+        void OnDisable()
+        {
+            StopRecording();
+        }
+    }
+}
